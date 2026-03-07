cf2b6b880d3bd991faf1077c333ef84f2d9d6b93
Dien Michael Laurente <dienml05@gmail.com>
2026-03-07 18:38:30 +0800

    Initial commit

---
+++ laundryhub/src/main/java/edu/cit/laurente/laundryhub/LaundryhubApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.laurente.laundryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryhubApplication.class, args);
+	}
+
+}
---
+++ laundryhub/src/test/java/edu/cit/laurente/laundryhub/LaundryhubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.laurente.laundryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
